c1f6adb75ba3765dfb3466159284dedb9ef29632
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2022-03-16 02:04:26 +0000

    dynamic delimiters

---
+++ CodeChallengeArea/GenericLinkedList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallengeArea
+{
+    public class GenericLinkedList<T>
+    {
+        private Node _head = null;
+        private List<Node> _nodeList = new();
+
+        private class Node
+        {
+            public Node Next;
+            public T Data;
+            public Node Previous;
+        }
+
+        public void AddNode(T t)
+        {
+            var newNode = new Node
+            {
+                Next = _head,
+                Data = t
+            };
+
+            _head = newNode;
+
+            //_nodeList.Add(newNode);
+        }
+
+        public void InsertAtStart(T t)
+        {
+
+        }
+
+        public void InsertInTheMiddle(T t)
+        {
+
+        }
+
+        public void InsertAtTheEnd(T t)
+        {
+            var newNode = new Node
+            {
+                Next = _head,
+                Data = t
+            };
+
+            _head = newNode;
+        }
+
+        public void RemoveNode(T t)
+        {
+            var node = _nodeList.Find(x => x.Data.Equals(t));
+            var position = _nodeList.FindIndex(0, x => x.Data.Equals(t));
+            var previousNode = new Node();
+
+            for (int i = 0; i < _nodeList.Count; i++)
+            {
+                if (i == position - 1)
+                {
+                    previousNode = _nodeList.ToArray()[i];
+                }
+            }
+
+            previousNode.Next = node.Next;
+            node.Next = null;
+            _head = previousNode;
+
+            _nodeList.Remove(node);
+        }
+
+        public void PrintList()
+        {
+            //foreach (var node in _nodeList)
+            //{
+            //    Console.WriteLine(node.Data.ToString());
+            //}
+
+            Node current = _head;
+
+            while (current != null)
+            {
+                Console.WriteLine(current.Data.ToString());
+                current = current.Next;
+            }
+        }
+    }
+}
--- CodeChallengeArea/StringCalculator.cs
+++ CodeChallengeArea/StringCalculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CodeChallengeArea
@@ -15,32 +16,14 @@ namespace CodeChallengeArea
                 return 0;
             }
 
-            //var numbersArray = numbers.Split(new string[] { ",", "//", ";" }, StringSplitOptions.RemoveEmptyEntries);
-            var numbersArray = numbers.Split(new string[] { ",", "//", ";" , "*", "%", "\n", "***" }, StringSplitOptions.RemoveEmptyEntries);
-            var negativeNumbers = new List<int>();
-            var total = 0;
-            for (int i = 0; i < numbersArray.Length; i++)
-            {
-                if (int.TryParse(numbersArray[i].ToString(), out int number))
-                {                   
-                    if (number < 0)
-                    {
-                        negativeNumbers.Add(number);
-                    }
-
-                    if (number > 1000)
-                    {
-                        number = 0;
-                    }
-                    
-                    total += number;
-                }
-            }
+            var delimiters = GetDelimiters(numbers);
+            var numbersArray = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            var (negativeNumbers, total) = ValidateAndSumStringNumbers(numbersArray);
 
             if (negativeNumbers.Any())
             {
                 var negativesBuilder = new StringBuilder();
-                negativeNumbers.ForEach(x => negativesBuilder.AppendJoin(" ",x.ToString()));
+                negativeNumbers.ForEach(x => negativesBuilder.AppendJoin(" ", x.ToString()));
 
                 throw new ApplicationException($"Negatives not allowed: {negativesBuilder}");
             }
@@ -48,7 +31,26 @@ namespace CodeChallengeArea
             return total;
         }
 
-        private static (List<int>, int) ValidateStringNumbers(string[] numbersArray)
+        private static List<string> GetDelimiters(string numbers)
+        {
+            var delimiters = new List<string> { "\n" };
+            if (numbers.StartsWith("//"))
+            {
+                var dynamicDelimiters = numbers.Split("//", StringSplitOptions.RemoveEmptyEntries);
+                var matches = Regex.Matches(dynamicDelimiters[0], @"[\[].*[\]]");
+                //var matches = Regex.Matches(dynamicDelimiters[0], @"[\[].*[\]]|(.).*[\\n]1");
+                var newDelimiters = matches[0].ToString().Split(new string[] { "[", "]" }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var delimiter in newDelimiters)
+                {
+                    delimiters.Add(delimiter);
+                }
+            }
+
+            return delimiters;
+        }
+
+        private static (List<int>, int) ValidateAndSumStringNumbers(string[] numbersArray)
         {
             var negativeNumbers = new List<int>();
             var total = 0;
